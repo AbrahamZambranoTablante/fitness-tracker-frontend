9af5e8465c25220b5ac84da885f0c5d13530d0de
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-07-01 21:05:46 -0400

    a loooot stuff man

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+import NavBar from "./Components/NavBar";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index"
+import Show from "./Pages/Show"
+import New from "./Pages/New"
+import FourOFour from "./Pages/FourOFour"
+import Edit from "./Pages/Edit"
+
+const API = import.meta.env.VITE_API_URL;
+
+function App() {
+  
+  useEffect(() => {
+    fetch(`${API}/workouts`)
+    .then((res) => {
+      return res.json()
+    })
+    .then(resJSON => console.log(resJSON))
+  }, [])
+
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={ <Home /> } />
+            <Route path="/workouts" element={ <Index/> } />
+            <Route path="/workouts/:id" element={ <Show /> } />
+            <Route path="/workouts/:id/edit" element={ <Edit/> } />
+            <Route path="/workouts/new" element={ <New /> } />
+            <Route path="*" element={ <FourOFour /> } />
+          </Routes>
+        </main>
+      </Router>
+    </>
+  )
+}
+
+export default App
