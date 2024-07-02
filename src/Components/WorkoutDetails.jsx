fd3fb9dd353d58d57acdf5c742fcb83650cd372c
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-07-01 21:47:25 -0400

    /workoutdetails -> 1. made fetch to get details about single workout 2. create state to store info about the workout 3. rendered the data from the state

--- src/Components/WorkoutDetails.jsx
+++ src/Components/WorkoutDetails.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 
 const WorkoutDetails = () => {
+  let navigate = useNavigate(); 
+
+  const [workout, setWorkout] = useState({
+    "type": "",
+"durationInMinutes": 0,
+"caloriesBurned": 0,
+"date": ""
+  });
+
+  const API = import.meta.env.VITE_API_URL;
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`${API}/workouts/${id}`)
+    .then(res => res.json())
+    .then(resJSON => setWorkout(resJSON))
+    .catch(() => {
+      navigate("/not-found")
+    })
+  }, [id, navigate])
+
   return (
-    <div>WorkoutDetails</div>
+    <>
+      <h1>WorkoutDetails</h1>
+      <h2>Workout:<br/> {workout.type}</h2>
+      <p>Minutes: {workout.durationInMinutes}</p>
+      <p>Calories Burned: {workout.caloriesBurned}</p>
+    </>
   )
 }
 
