6c9189627af0b6dff98f0bd01cce61637f4c6548
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-07-01 22:00:08 -0400

    workoutNewForm -> 1. added a button in navbar to go to new view 2. created form for adding a new workout

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+      NavBar
+      <button className=""><Link to="/workouts/new">New Workout</Link></button>
+    </>
   )
 }
 
--- src/Components/WorkoutNewForm.jsx
+++ src/Components/WorkoutNewForm.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const WorkoutNewForm = () => {
+
+  const [workout, setWorkout] = useState({})
+
   return (
-    <div>WorkoutNewForm</div>
+    <>
+      WorkoutNewForm
+      <form className="">
+        <label htmlFor="" className="">Type: </label>
+        <input id="type" type="text" value={WorkoutNewForm.type} required/>
+        <label htmlFor="" className="">Duration: </label>
+        <input id="duration" type="number" value={workout.durationInMinutes} required/>
+        <label htmlFor="" className="">Calories Burned: </label>
+        <input id="caloriesBurned" type="number" value={workout.caloriesBurned} required/>
+        <label htmlFor="" className="">Date: </label>
+        <input id="date" type="text" value={workout.date} required/>
+      </form>
+    </>
   )
 }
 
